9f0c80c352c4708937629b85db92101f624cdfc7
Tobías Uriel Videla Guliotti <tobiasvidelaug14@gmail.com>
2023-12-03 07:38:13 -0300

    Agregar menu con Título

--- src/main.c
+++ src/main.c
@@ -3,9 +3,12 @@
 
 #include "../include/utilidades.h" //Ruta relativa
 #include "../include/utilidades.c" //Ruta relativa
+#include "menu.h" //Ruta relativa
+#include "menu.c" //Ruta relativa
 
 int main(void) {
     limpiarConsola();
+    menu();
     continuar();
     printf("\nPrograma terminado.\n\n");
     return 0;
---
+++ src/menu.c
@@ -0,0 +1,32 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/utilidades.h"
+#include "../include/utilidades.c"
+#include "menu.h"
+
+
+void menu() {
+    int opcion;
+    do {
+        titulo();
+        imprimirOpciones();
+        opcion = LeerEntero();
+    } while (opcion != 0);
+
+}
+
+void titulo() {
+    imprimirCadenaCentrada("\n");
+    imprimirCadenaCentrada("Sistema Integral para TechSprint Innovations");
+    imprimirCadenaCentrada("\n");
+}
+
+void imprimirOpciones() {
+    printf("[1] Crear archivo binario para Empleados y Cargar Datos.\n");
+    printf("[2] Crear archivo binario para Proveedores y Cargar Datos\n");
+    printf("[3] Crear archivo binario para Recursos y Cargar Datos\n");
+    printf("[4] Crear archivo binario para Ventas y Cargar Datos\n");
+    printf("[0] Salir\n");
+}
