08acb1b0c496d55b27593acb39caf8180fd3ae44
Tobías Uriel Videla Guliotti <tobiasvidelaug14@gmail.com>
2023-12-03 06:03:37 -0300

    anexo de una fuente en un comentario

--- include/utilidades.c
+++ include/utilidades.c
@@ -13,10 +13,12 @@ void limpiarConsola() {
 
 void limpiarBuffers() {
     scanf("%*[^\n]%*c");
-    /* se le dice a scanf primero que ignore (uso de *) cualquier número de no 
+    /* "se le dice a scanf primero que ignore (uso de *) cualquier número de no 
     caracteres en blanco (distinto a tabulador '\t', espacio ' ' o salto de línea '\n') 
     con el especificador %*[^\n] hasta que encuentre un carácter en blanco (el salto de línea),
-    en cuyo caso lo ignora con el %*c. */
+    en cuyo caso lo ignora con el %*c."
+    Source: https://es.stackoverflow.com/a/81435
+    */
 }
 
 void continuar() {
